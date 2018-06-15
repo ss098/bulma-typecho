ae1c02e51252128c7235baa6308da71831ddf87d
cenegd <cenegd@live.com>
2018-06-15 23:12:28 +0800

    remove link

--- archive.php
+++ archive.php
@@ -5,33 +5,6 @@
         <article>
             <section class="section">
                 <a class="article-title" href="<?php $this->permalink() ?>"><?php $this->title() ?></a>
-
-                <div class="field is-grouped is-grouped-multiline" style="margin: 2em 0;">
-                    <div class="control">
-                        <p class="tags has-addons">
-                            <span class="tag is-info">分类</span>
-                            <span class="tag"><?php $this->category('，', true, '无') ?></span>
-                        </p>
-                    </div>
-                    <div class="control">
-                        <p class="tags has-addons">
-                            <span class="tag is-info">分类</span>
-                            <span class="tag"><?php $this->category('，', true, '无') ?></span>
-                        </p>
-                    </div>
-                    <div class="control">
-                        <p class="tags has-addons">
-                            <span class="tag is-info">作者</span>
-                            <a class="tag" href="<?php $this->author->permalink(); ?>"><?php $this->author(); ?></a>
-                        </p>
-                    </div>
-                    <div class="control">
-                        <p class="tags has-addons">
-                            <span class="tag is-info">时间</span>
-                            <span class="tag"><time datetime="<?php $this->date('c'); ?>"><?php $this->date(); ?></time></span>
-                        </p>
-                    </div>
-                </div>
             </section>
 
             <section class="section content">
